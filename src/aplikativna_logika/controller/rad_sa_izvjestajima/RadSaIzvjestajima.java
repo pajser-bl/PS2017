bb656c2a8463c2295d2b696d8499110b8e4fc36d
Marina <Marina@Marina-PC>
2018-06-26 23:36:11 +0200

    ..

---
+++ src/aplikativna_logika/controller/rad_sa_izvjestajima/RadSaIzvjestajima.java
@@ -0,0 +1,16 @@
+package aplikativna_logika.controller.rad_sa_izvjestajima;
+
+public class RadSaIzvjestajima {
+	
+	public void noviIzvjestaj() {
+		
+	}
+	
+	public void pregledajIzvjestaj() {
+		
+	}
+	
+	public void pregledajIzvjestaje() {
+		
+	}
+}
