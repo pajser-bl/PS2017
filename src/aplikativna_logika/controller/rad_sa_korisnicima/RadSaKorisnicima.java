821e7fe42d6772711aa3fa0a91f5e02e9bb69b55
pajser-bl <pajser.bl@hotmail.com>
2018-08-27 16:44:57 +0200

    ooop

--- src/aplikativna_logika/controller/rad_sa_korisnicima/RadSaKorisnicima.java
+++
@@ -1,5 +0,0 @@
-package aplikativna_logika.controller.rad_sa_korisnicima;
-
-public class RadSaKorisnicima {
-//test.test.
-}
--- src/util/Vrijeme.java
+++ src/util/Vrijeme.java
@@ -1,4 +1,4 @@
-package aplikativna_logika.util;
+package util;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
