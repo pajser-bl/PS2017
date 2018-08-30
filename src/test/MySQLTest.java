06e7d427c345d9887ee8f12b0e44b3635ccf1994
Marina <Marina@DESKTOP-VBV0EQJ>
2018-08-30 15:54:32 +0200

    ccccc

--- src/test/MySQLTest.java
+++ src/test/MySQLTest.java
@@ -8,6 +8,9 @@ import DAO.SessionDAO;
 import DAO.SubscriptionDAO;
 import DAO.UserDAO;
 import DAO.VehicleDAO;
+import client.Client;
+import model.users.User;
+import utility.TimeUtility;
 import DAO.MySQLClientDAO;
 import DAO.MySQLCredentialsDAO;
 import DAO.MySQLEventDAO;
@@ -28,19 +31,31 @@ import DAO.InterventionDAO;
 
 public class MySQLTest {
 	public static void main(String [] args) {
-		UserDAO user = new MySQLUserDAO();
-		ClientDAO client = new MySQLClientDAO();
-		SubscriptionDAO sub = new MySQLSubscriptionDAO();
-		CredentialsDAO cred = new MySQLCredentialsDAO();
-		EventDAO event = new MySQLEventDAO();
-		SessionDAO session = new MySQLSessionDAO();
-		InterventionCoordinateDAO coordinate = new MySQLInterventionCoordinateDAO();
-		InterventionDAO intervention = new MySQLInterventionDAO();
-		ReportDAO report = new MySQLReportDAO();
-		RoadReportDAO roadReport = new MySQLRoadReportDAO();
-		VehicleDAO vehicle = new MySQLVehicleDAO();
+		UserDAO userDAO = new MySQLUserDAO();
+		ClientDAO clientDAO = new MySQLClientDAO();
+		SubscriptionDAO subDAO = new MySQLSubscriptionDAO();
+		CredentialsDAO credDAO = new MySQLCredentialsDAO();
+		EventDAO eventDAO = new MySQLEventDAO();
+		SessionDAO sessionDAO = new MySQLSessionDAO();
+		InterventionCoordinateDAO coordinateDAO = new MySQLInterventionCoordinateDAO();
+		InterventionDAO interventionDAO = new MySQLInterventionDAO();
+		ReportDAO reportDAO = new MySQLReportDAO();
+		RoadReportDAO roadReportDAO = new MySQLRoadReportDAO();
+		VehicleDAO vehicleDAO = new MySQLVehicleDAO();
 		
-		user.insert(new User("A", "A", 1, new LocalDate(1990, 11, 1), "vss");
+		userDAO.insert(new User("A", "A", 1, TimeUtility.stringToLocalDate("1999-12-10") , "vss"));
+		userDAO.select(1);
+		System.out.println(userDAO);
+		
+		
+		clientDAO.insert(new Client(2,"Marko", "Markovic", "65656656565"));
+		clientDAO.insert(new Client(3, "M", "M", "2737747"));
+		clientDAO.update(new Client(2,"Marko", "Markovic", "565757575757"));
+		
+		
+		
+		
+		
 		
 		
 		
