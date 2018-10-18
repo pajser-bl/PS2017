27f227ac1402cfee531f4a43aa6cdf039723b180
pajser-bl <pajser.bl@hotmail.com>
2018-10-18 14:48:05 +0200

    TimeUtility popravljen ponovo

--- src/utility/TimeUtility.java
+++ src/utility/TimeUtility.java
@@ -6,35 +6,41 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtility {
-	
-public static String getStringTimeNow() {
+
+	public static String getStringTimeNow() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		return formatter.format(LocalDateTime.now());
 	}
-	public static String formatDate(LocalDateTime localDateTime) {
+
+	public static String localDateTimeToDDMMYYYY(LocalDateTime localDateTime) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
 		return formatter.format(localDateTime);
 	}
+
 	public static String formatTime(LocalDateTime localDateTime) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 		return formatter.format(localDateTime);
-		}
+	}
+
 	public static LocalDateTime stringToLocalDateTime(String stringDateTime) {
-		if(stringDateTime==null)return null;
+		if (stringDateTime == null)
+			return null;
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		return LocalDateTime.parse(stringDateTime, formatter);
 	}
+
 	public static String localDateTimeToString(LocalDateTime localDateTime) {
-		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-			return formatter.format(localDateTime);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		return formatter.format(localDateTime);
 	}
+
 	public static LocalDate stringToLocalDate(String stringDate) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		return LocalDate.parse(stringDate, formatter);
 	}
-	
+
 	public static void main(String args[]) {
 		System.out.println(TimeUtility.getStringTimeNow());
-		
+
 	}
 }
