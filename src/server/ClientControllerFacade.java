058649a51c4360b7022d49915d06c2fe78962024
pajser-bl <pajser.bl@hotmail.com>
2018-11-06 15:59:06 +0100

    josss

--- src/server/ClientControllerFacade.java
+++ src/server/ClientControllerFacade.java
@@ -31,6 +31,7 @@ import model.users.Subscription;
 import model.users.User;
 import server.controller.AccessControl;
 import server.controller.ClientControl;
+import server.controller.SubscriptionControl;
 import server.controller.UserControl;
 import utility.HashHandler;
 import utility.TimeUtility;
@@ -141,55 +142,21 @@ public class ClientControllerFacade {
 	// public void viewClients(String param){}
 
 	public ArrayList<String> viewSubscription(int subscription_ID) {
-		ArrayList<String> reply = new ArrayList<>();
-		Subscription subscription = subscriptionDAO.select(subscription_ID);
-
-		if (subscription != null) {
-			reply.add("VIEW SUBSCRIPTION OK");
-			reply.add("" + subscription.getID_subscription());
-			reply.add("" + subscription.getID_client());
-			reply.add(TimeUtility.localDateToString(subscription.getStart_date()));
-			reply.add(TimeUtility.localDateToString(subscription.getEnd_date()));
-		} else {
-			reply.add("VIEW subscription FAILED");
-		}
-		return reply;
+		return SubscriptionControl.viewSubscription(subscription_ID, subscriptionDAO);
 	}
 	// public void viewSubscriptions(String param){}
 
 	public ArrayList<String> newSubscription(String client_ID, String start_date, String end_date) {
-		ArrayList<String> reply = new ArrayList<>();
-		Subscription subscription = new Subscription(Integer.parseInt(client_ID), Integer.parseInt(client_ID),
-				TimeUtility.stringToLocalDate(start_date), TimeUtility.stringToLocalDate(end_date));
-		if (subscriptionDAO.insert(subscription) != 0) {
-			reply.add("NEW SUBSCRIPTION OK");
-		} else {
-			reply.add("NEW SUBSCRIPTION FAILED");
-		}
-		return reply;
+		return SubscriptionControl.newSubscription(client_ID, start_date, end_date, subscriptionDAO);
 	}
 
 	public ArrayList<String> updateSubscription(String subscription_ID, String client_ID, String start_date,
 			String end_date) {
-		ArrayList<String> reply = new ArrayList<>();
-		Subscription subscription = new Subscription(Integer.parseInt(subscription_ID), Integer.parseInt(client_ID),
-				TimeUtility.stringToLocalDate(start_date), TimeUtility.stringToLocalDate(end_date));
-		if (subscriptionDAO.update(subscription) != 0) {
-			reply.add("UPDATE SUBSCRIPTION OK");
-		} else {
-			reply.add("UPDATE SUBSCRIPTION FAILED");
-		}
-		return reply;
+		return SubscriptionControl.updateSubscription(subscription_ID, client_ID, start_date, end_date, subscriptionDAO);
 	}
 
 	public ArrayList<String> deleteSubscription(String subscription_ID) {
-		ArrayList<String> reply = new ArrayList<>();
-		if (subscriptionDAO.delete(Integer.parseInt(subscription_ID)) != 0) {
-			reply.add("DELETE SUBSCRIPTION OK");
-		} else {
-			reply.add("DELETE SUBSCRIPTION FAILED");
-		}
-		return reply;
+		return SubscriptionControl.deleteSubscription(subscription_ID, subscriptionDAO);
 	}
 
 	public ArrayList<String> newIntervention(Intervention intervention) {
---
+++ src/server/controller/SubscriptionControl.java
@@ -0,0 +1,59 @@
+package server.controller;
+
+import java.util.ArrayList;
+
+import DAO.SubscriptionDAO;
+import model.users.Subscription;
+import utility.TimeUtility;
+
+public class SubscriptionControl {
+	
+	
+	public static ArrayList<String> viewSubscription(int subscription_ID, SubscriptionDAO subscriptionDAO) {
+		ArrayList<String> reply = new ArrayList<>();
+		Subscription subscription = subscriptionDAO.select(subscription_ID);
+
+		if (subscription != null) {
+			reply.add("VIEW SUBSCRIPTION OK");
+			reply.add("" + subscription.getID_subscription());
+			reply.add("" + subscription.getID_client());
+			reply.add(TimeUtility.localDateToString(subscription.getStart_date()));
+			reply.add(TimeUtility.localDateToString(subscription.getEnd_date()));
+		} else {
+			reply.add("VIEW subscription FAILED");
+		}
+		return reply;
+	}
+	public static ArrayList<String> newSubscription(String client_ID, String start_date, String end_date, SubscriptionDAO subscriptionDAO) {
+		ArrayList<String> reply = new ArrayList<>();
+		Subscription subscription = new Subscription(Integer.parseInt(client_ID), Integer.parseInt(client_ID),
+				TimeUtility.stringToLocalDate(start_date), TimeUtility.stringToLocalDate(end_date));
+		if (subscriptionDAO.insert(subscription) != 0) {
+			reply.add("NEW SUBSCRIPTION OK");
+		} else {
+			reply.add("NEW SUBSCRIPTION FAILED");
+		}
+		return reply;
+	}
+	public static ArrayList<String> updateSubscription(String subscription_ID, String client_ID, String start_date,
+			String end_date, SubscriptionDAO subscriptionDAO) {
+		ArrayList<String> reply = new ArrayList<>();
+		Subscription subscription = new Subscription(Integer.parseInt(subscription_ID), Integer.parseInt(client_ID),
+				TimeUtility.stringToLocalDate(start_date), TimeUtility.stringToLocalDate(end_date));
+		if (subscriptionDAO.update(subscription) != 0) {
+			reply.add("UPDATE SUBSCRIPTION OK");
+		} else {
+			reply.add("UPDATE SUBSCRIPTION FAILED");
+		}
+		return reply;
+	}
+	public static ArrayList<String> deleteSubscription(String subscription_ID, SubscriptionDAO subscriptionDAO) {
+		ArrayList<String> reply = new ArrayList<>();
+		if (subscriptionDAO.delete(Integer.parseInt(subscription_ID)) != 0) {
+			reply.add("DELETE SUBSCRIPTION OK");
+		} else {
+			reply.add("DELETE SUBSCRIPTION FAILED");
+		}
+		return reply;
+	}
+}
